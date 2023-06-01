ead2e6146321a255074812a7fcac3bdd5f15cc4d
Emmanuel Giraldo <esemmanuel2022@gmail.com>
2023-06-01 02:50:49 -0500

    scss-module&styles&order

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,18 @@
+// Filter.js
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Filter.module.scss';
+
+const Filter = ({ value, onChange }) => (
+  <label className={styles.filter}>
+    Filter contacts by name:
+    <input type="text" value={value} onChange={onChange} className={styles.input} />
+  </label>
+);
+
+Filter.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+export default Filter;
