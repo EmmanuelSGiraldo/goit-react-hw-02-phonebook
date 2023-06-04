2202a3d896b936ec8b1d4bd0b6d9c45c558069be
Emmanuel Giraldo <esemmanuel2022@gmail.com>
2023-06-03 23:14:10 -0500

    changes funtions to class components

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,29 +1,36 @@
-// ContactList.js
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './ContactList.module.scss';
 
-const ContactList = ({ contacts, onDeleteContact }) => (
-  <ul className={styles.contactList}>
-    {contacts.map((contact) => (
-      <li key={contact.id} className={styles.contactItem}>
-        <span className={styles.contactName}>{contact.name}: </span>
-        <span className={styles.contactNumber}>{contact.number}</span>
-        <button onClick={() => onDeleteContact(contact.id)} className={styles.deleteButton}>Delete</button>
-      </li>
-    ))}
-  </ul>
-);
+class ContactList extends Component {
+  render() {
+    const { contacts, onDeleteContact } = this.props;
+
+    return (
+      <ul className={styles.contactList}>
+        {contacts.map((contact) => (
+          <li key={contact.id} className={styles.contactItem}>
+            <span className={styles.contactName}>{contact.name}: </span>
+            <span className={styles.contactNumber}>{contact.number}</span>
+            <button onClick={() => onDeleteContact(contact.id)} className={styles.deleteButton}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
 ContactList.propTypes = {
   contacts: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired
+      number: PropTypes.string.isRequired,
     })
   ).isRequired,
-  onDeleteContact: PropTypes.func.isRequired
+  onDeleteContact: PropTypes.func.isRequired,
 };
 
-export default ContactList;
+export default ContactList ;
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,23 @@
-// Filter.js
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Filter.module.scss';
 
-const Filter = ({ value, onChange }) => (
-  <label className={styles.filter}>
-    Filter contacts by name:
-    <input type="text" value={value} onChange={onChange} className={styles.input} />
-  </label>
-);
+class Filter extends Component {
+  render() {
+    const { value, onChange } = this.props;
+
+    return (
+      <label className={styles.filter}>
+        Filter contacts by name:
+        <input type="text" value={value} onChange={onChange} className={styles.input} />
+      </label>
+    );
+  }
+}
 
 Filter.propTypes = {
   value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired
+  onChange: PropTypes.func.isRequired,
 };
 
 export default Filter;
